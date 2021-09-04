73893d4ba663fbe71ce8c09d88cebba3811b42b3
Aviv Haruzi <avivharuzi@gmail.com>
2021-09-04 21:20:47 +0300

    :art:  format files

--- apps/csp-demo/src/app/app-routing.module.ts
+++ apps/csp-demo/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('@angular-web-security/csp-demo/feature-home').then(
+        ({ CspDemoFeatureHomeModule }) => CspDemoFeatureHomeModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- libs/csp-demo/feature-home/src/lib/home.component.ts
+++ libs/csp-demo/feature-home/src/lib/home.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'csp-demo-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  scriptInjection = `<div>Trying to fetch</div><img src="" onerror="fetch('https://jsonplaceholder.typicode.com/posts')" />`;
+  safeScriptInjection: SafeHtml;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private domSanitizer: DomSanitizer) {
+    this.safeScriptInjection = this.domSanitizer.bypassSecurityTrustHtml(
+      this.scriptInjection
+    );
   }
-
 }
